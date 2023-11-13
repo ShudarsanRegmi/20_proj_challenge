54a06eb02cb8846909303241e1a4b32db1d90d6a
ShudarsanRegmi <shudarsanregmi555@gmail.com>
2023-11-13 05:47:05 +0530

    rock paper scissor was updated

--- rock_paper_scissor/main.c
+++ rock_paper_scissor/main.c
@@ -1,65 +1,104 @@
 #include <stdio.h>
-int checkWinner(char user, char computer) {
+#include <time.h>
+#include <stdlib.h>
+int checkWinner(int user, int computer) {
 
-
+	// 0 --> rock
+	// 1 --> paper
+	// 2 --> Scissor
+	
 	if(user == computer) {
-		printf("tie");
+		printf("tie\n");
 		return -1; //  -1 signifies tie
 	}
     // for RS -> R is the winner
-	if ((user == 'R' && computer == 'S') || (user =='S' && computer == 'R') ) {
+	if ((user == 0 && computer == 2) || (user == 2 && computer == 0) ) {
 		// rock is winner
 
-		if(user == 'R') {
-		    printf("user is winner");
+		if(user == 0) {
+			printf("user is winner");
+			return 0;
 			// user is winner
 		}else{
 			// computer is winner
-        printf("computer is winner");
+			printf("computer is winner");
+			return 1;
 
 		}
 
 		// RP --> paper winner
-	}else if ((user == 'R' && computer == 'P') || (user == 'P' && computer == 'R')){
+	}else if ((user == 0 && computer == 1) || (user == 1 && computer == 0)){
         // paper is winner
-        if(user == 'P') {
+        if(user == 1) {
 		    printf("user is winner");
+		    return 0;
 			// user is winner
 		}else{
 			// computer is winner
             printf("computer is winner");
+			return 1;
 
 		}
       // PS --> Scissor is winner  
-	}else if ((user == 'P' && computer == 'S') || (user == 'S' && computer == 'P')){
+	}else if ((user == 1 && computer == 2) || (user == 2 && computer == 1)){
         // scissor is winner
-        if(user == 'S') {
+        if(user == 2) {
 		    printf("user is winner");
 			// user is winner
+		return 0;
 		}else{
 			// computer is winner
             printf("computer is winner");
+	    return 1;
 
 		}
 	    
 	    
 	}else{
 	    printf("Unhandeled Exception");
+	    return -1;
 	}
 
 
 }
-
 int main() {
 	// rock = 0
+	int userscore = 0;
+	int computerscore;
+	int userchoice;
+	int winner;
+	int computerchoice;
+	srand(time(NULL));
 	while(1) {
 	// main game loop
 		// computer generate
+		computerchoice = rand()%3;
 		
 		// asking user
+		do {
+			printf("\nPress 1 for Rock \nPress 2 for Paper \nPress 3 for Scissor\n");
+			scanf("%d",&userchoice);
+			
+		} while (!(userchoice == 1 || userchoice == 2 || userchoice == 3));
+
+
+		printf("You: %d\n",userchoice);
+		printf("Computer: %d\n",computerchoice);	
+	
+		winner = checkWinner(userchoice-1, computerchoice);
+	//int winner = 0;
+
+		if(winner == 0) {
+			printf("Your Score: %d\n",++userscore);
+		}else if(winner == 1) {
+			printf("Computer Score: %d\n",++computerscore);
+		}else if(winner == -1) {
+			printf("Game tie\n");
+		}else{
+			printf("Unhandeled exception\n");
+			return 1;
+		}
 		
-	} // end game loop
-	
-	
+		} // end game loop
 	return 0;
 }
